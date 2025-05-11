18d06768d04f3ac0cea6583a8e973ccf89289543
itstrH <trannguyenbahuy2908@gmail.com>
2025-05-11 16:54:39 +0700

    deployment test 2

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: "./src", 
+  // root: "./src", 
   base: "./",
-  plugins: [zaloMiniApp(), react()],
+  plugins: [zaloMiniApp({
+    autoGenerateConfig: true,
+  }), react()],
   build: {
     outDir: "../dist", 
     emptyOutDir: true,
