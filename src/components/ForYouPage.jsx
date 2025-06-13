8051e158c5acce199950cb5653c44f32deb6b052
itstrH <trannguyenbahuy2908@gmail.com>
2025-06-13 07:56:04 +0700

    updated UI

--- src/components/ForYouPage.jsx
+++ src/components/ForYouPage.jsx
@@ -73,16 +73,26 @@ const ForYouPage = () => {
                                 >
                                     {event.event_name}
                                 </Text.Title>
-                                <Text className="text-sm text-gray-600 line-clamp-1">
-                                    📅 {new Date(event.event_date).toLocaleDateString()}
+                                <Text className="text-sm text-gray-600 line-clamp-1 flex items-center gap-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
+</svg>
+ {new Date(event.event_date).toLocaleDateString()}
                                 </Text>
                                 {event.event_time && (
-                                    <Text className="text-sm text-gray-600 line-clamp-1">
-                                        🕒 {event.event_time}
+                                    <Text className="text-sm text-gray-600 line-clamp-1 flex items-center gap-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+</svg>
+{event.event_time}
                                     </Text>
                                 )}
-                                <Text className="text-sm text-gray-600 line-clamp-1">
-                                    📍 {event.event_location}
+                                <Text className="text-sm text-gray-600 line-clamp-1 flex items-center gap-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+</svg>
+{event.event_location}
                                 </Text>
                             </Box>
                         </Box>
